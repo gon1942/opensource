ab3d7d20f4eae3ca84144bea1117c4c9274b6593
김경곤 <kg1942@invesume.com>
2023-07-12 14:53:22 +0900

    test

--- test.js
+++ test.js
@@ -1,3 +1,6 @@
-test("1 is 1", () => {
-  expect(1).toBe(1);
-});
+const foo = () => {
+  const a = [1, 2, 3]; // 스코프 내부 작성 시 두 공백 들여쓰기
+  console.log("a is " + a);
+};
+// <= 빈 줄이 한 줄 이상 안됨.
+foo();
